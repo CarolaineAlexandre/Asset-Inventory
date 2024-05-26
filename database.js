76539d4ea3f739ef4c55acaa2ca5a5214164f041
Carolaine Alexandre <carolaine.ferrazemp@gmail.com>
2024-05-26 11:25:05 -0300

    chore: update database

--- database.js
+++ database.js
@@ -1,5 +1,6 @@
 const express = require('express');
-const { MongoClient, ObjectId } = require('mongodb');
+const { MongoClient } = require('mongodb');
+const { ObjectId } = require('mongodb');
 
 const app = express();
 app.use(express.json());
@@ -16,12 +17,7 @@ async function connectToDB() {
     try {
         client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
         await client.connect();
-        console.log('Conectado ao MongoDB na porta:', port);
-
-        // Buscar todas as informações da coleção após conectar
-        const db = client.db(dbName);
-        const funcionarios = await db.collection(collectionName).find().toArray();
-        console.log('Todos os funcionários:', funcionarios);
+        console.log('Conectado ao MongoDB');
     } catch (err) {
         console.error('Erro ao conectar ao MongoDB:', err);
     }
@@ -29,80 +25,252 @@ async function connectToDB() {
 
 connectToDB();
 
-// Inserir um novo funcionário
+// Consultar todos os funcionários
+app.get('/funcionarios', async (req, res) => {
+    try {
+        const db = client.db(dbName);
+        const result = await db.collection(collectionName).find().toArray();
+        res.status(200).json(result);
+    } catch (err) {
+        console.error('Erro ao consultar funcionários:', err);
+        res.status(500).send('Erro ao consultar funcionários');
+    }
+});
+
+// Criar novo funcionário
 app.post('/funcionario', async (req, res) => {
     try {
         const db = client.db(dbName);
         const result = await db.collection(collectionName).insertOne(req.body);
-        res.status(201).json(result.ops[0]);
+
+        if (result.acknowledged) {
+            const novoFuncionario = {
+                _id: result.insertedId,
+                ...req.body
+            };
+            console.log('Novo funcionário inserido');
+            res.status(201).json(novoFuncionario);
+        } else {
+            throw new Error('Nenhum documento inserido.');
+        }
     } catch (err) {
-        console.error('Erro ao inserir funcionário:', err);
-        res.status(500).send('Erro ao inserir funcionário');
+        console.error('Erro ao criar novo funcionário:', err);
+        res.status(500).send('Erro ao criar novo funcionário.');
     }
 });
 
 // Excluir um funcionário
 app.delete('/funcionario/:id', async (req, res) => {
+    const funcionarioId = req.params.id;
+
     try {
         const db = client.db(dbName);
-        const result = await db.collection(collectionName).deleteOne({ _id: ObjectId(req.params.id) });
+        const funcionario = await db.collection(collectionName).findOne({ _id: new ObjectId(funcionarioId) });
+
+        if (!funcionario) {
+            res.status(404).send(`Funcionário com ID ${funcionarioId} não encontrado.`);
+            return;
+        }
+
+        // Um funcionário só pode ser excluído se não houver nenhum ativo configurado para ele
+        const ativosConfigurados = [
+            funcionario.Monitor1.Presente,
+            funcionario.Monitor2.Presente,
+            funcionario.Teclado.Presente,
+            funcionario.Mouse.Presente,
+            funcionario.Desktop.Presente,
+            funcionario.Nobreak.Presente,
+            funcionario.Headset.Presente,
+            funcionario.Celular.Presente,
+            funcionario.Acessórios.Presente
+        ].some(presente => presente === "SIM");
+
+        if (ativosConfigurados) {
+            res.status(400).send(`Funcionário com ID ${funcionarioId} não pode ser deletado pois possui ativos configurados.`);
+            return;
+        }
+
+        const result = await db.collection(collectionName).deleteOne({ _id: new ObjectId(funcionarioId) });
+
         if (result.deletedCount === 1) {
-            res.status(204).send();
+            console.log(`Funcionário com ID ${funcionarioId} deletado.`);
+            res.status(200).send(`Funcionário com ID ${funcionarioId} deletado.`);
         } else {
-            res.status(404).send('Funcionário não encontrado');
+            console.log(`Funcionário com ID ${funcionarioId} não encontrado.`);
+            res.status(404).send(`Funcionário com ID ${funcionarioId} não encontrado.`);
         }
     } catch (err) {
-        console.error('Erro ao excluir funcionário:', err);
-        res.status(500).send('Erro ao excluir funcionário');
+        console.error('Erro ao deletar o funcionário:', err);
+        res.status(500).send('Erro ao deletar o funcionário.');
     }
 });
 
-// Listar todos os funcionários
-app.get('/funcionario', async (req, res) => {
-    try {
-        const db = client.db(dbName);
-        const funcionarios = await db.collection(collectionName).find();
-        res.json(funcionarios);
-    } catch (err) {
-        console.error('Erro ao listar funcionários:', err);
-        res.status(500).send('Erro ao listar funcionários');
-    }
-});
-
-// Consultar o inventário completo de um determinado funcionário
-app.get('/funcionario/:id', async (req, res) => {
-    try {
-        const db = client.db(dbName);
-        const funcionario = await db.collection(collectionName).findOne({ _id: ObjectId(req.params.id) });
-        if (funcionario) {
-            res.json(funcionario);
-        } else {
-            res.status(404).send('Funcionário não encontrado');
-        }
-    } catch (err) {
-        console.error('Erro ao consultar funcionário:', err);
-        res.status(500).send('Erro ao consultar funcionário');
-    }
-});
 
 // Atualizar o nome do funcionário tendo como referência do documento o próprio CPF
-app.put('/funcionario/:cpf', async (req, res) => {
+app.put('/funcionario/nome', async (req, res) => {
+    const { CPF, newName } = req.body;
+
+    if (!CPF || !newName) {
+        res.status(400).send('CPF e novoNome são obrigatórios.');
+        return;
+    }
+
     try {
         const db = client.db(dbName);
-        const result = await db.collection(collectionName).updateOne({ CPF: req.params.cpf }, { $set: { Nome: req.body.Nome } });
+        const result = await db.collection(collectionName).updateOne(
+            { CPF: CPF },
+            { $set: { Name: newName } }
+        );
+
+        if (result.matchedCount === 0) {
+            res.status(404).send(`Funcionário com CPF ${CPF} não encontrado.`);
+            return;
+        }
+
         if (result.modifiedCount === 1) {
-            res.status(200).send('Nome do funcionário atualizado');
+            console.log(`Nome do funcionário com CPF ${CPF} atualizado para ${newName}.`);
+            res.status(200).send(`Nome do funcionário com CPF ${CPF} atualizado para ${newName}.`);
         } else {
-            res.status(404).send('Funcionário não encontrado');
+            res.status(304).send('Nenhuma alteração feita no nome do funcionário.');
         }
     } catch (err) {
-        console.error('Erro ao atualizar nome do funcionário:', err);
-        res.status(500).send('Erro ao atualizar nome do funcionário');
+        console.error('Erro ao atualizar o nome do funcionário:', err);
+        res.status(500).send('Erro ao atualizar o nome do funcionário.');
     }
 });
 
 
-// Iniciar o servidor
+// Atualizar a informação de um ativo
+// app.put('/funcionario/:cpf/ativo/:ativo', async (req, res) => {
+//     const { cpf, ativo } = req.params;
+//     const novoAtivo = req.body;
+
+//     try {
+//         const db = client.db(dbName);
+//         const update = {};
+
+//         update[ativo] = novoAtivo;
+
+//         const result = await db.collection(collectionName).updateOne(
+//             { CPF: cpf },
+//             { $set: update }
+//         );
+
+//         if (result.matchedCount === 0) {
+//             res.status(404).send(`Funcionário com CPF ${cpf} não encontrado.`);
+//             return;
+//         }
+
+//         if (result.modifiedCount === 1) {
+//             res.status(200).send(`Ativo ${ativo} do funcionário com CPF ${cpf} atualizado.`);
+//         } else {
+//             res.status(304).send('Nenhuma alteração feita no ativo do funcionário.');
+//         }
+//     } catch (err) {
+//         console.error('Erro ao atualizar o ativo do funcionário:', err);
+//         res.status(500).send('Erro ao atualizar o ativo do funcionário.');
+//     }
+// });
+
+
+// // Função genérica para atualizar as informações de um ativo
+// async function updateAtivo(cpf, ativo, dadosAtivo) {
+//     const db = client.db(dbName);
+//     const update = {};
+//     update[ativo] = dadosAtivo;
+
+//     const result = await db.collection(collectionName).updateOne(
+//         { CPF: cpf },
+//         { $set: update }
+//     );
+
+//     return result;
+// }
+
+// // Função genérica para limpar as informações de um ativo
+// async function clearAtivo(cpf, ativo) {
+//     const db = client.db(dbName);
+//     const update = {};
+
+//     // Define as informações padrão para cada tipo de ativo
+//     switch (ativo) {
+//         case 'Monitor1':
+//         case 'Monitor2':
+//         case 'Teclado':
+//         case 'Mouse':
+//         case 'Headset':
+//         case 'Celular':
+//             update[ativo] = {
+//                 Presente: "NÃO",
+//                 Modelo: "",
+//                 'Número de Série': "",
+//                 Observação: ""
+//             };
+//             break;
+//         case 'Desktop':
+//             update[ativo] = {
+//                 Presente: "NÃO",
+//                 TAG: "",
+//                 Modelo: "",
+//                 'Número de Série': "",
+//                 Versão: "",
+//                 Características: "",
+//                 Observação: ""
+//             };
+//             break;
+//         case 'Acessórios':
+//             update[ativo] = {
+//                 Presente: "NÃO",
+//                 'Suporte notebook': "",
+//                 'Mouse pad': "",
+//                 Observação: ""
+//             };
+//             break;
+//         case 'Nobreak':
+//             update[ativo] = {
+//                 Presente: "NÃO",
+//                 Modelo: "",
+//                 'Número de Série': "",
+//                 Observação: ""
+//             };
+//             break;
+//         default:
+//             throw new Error('Ativo não reconhecido.');
+//     }
+
+//     const result = await db.collection(collectionName).updateOne(
+//         { CPF: cpf },
+//         { $set: update }
+//     );
+
+//     return result;
+// }
+
+// // Endpoints atualizados
+
+// app.put('/notebook/:cpf', async (req, res) => {
+//     try {
+//         const { cpf } = req.params;
+//         const { notebook } = req.body;
+//         const result = await updateAtivo(cpf, 'Notebook', notebook);
+//         res.status(200).json(result);
+//     } catch (error) {
+//         res.status(500).json({ message: 'Erro ao inserir notebook', error: error });
+//     }
+// });
+
+// app.put('/clear_notebook/:cpf', async (req, res) => {
+//     try {
+//         const { cpf } = req.params;
+//         const result = await clearAtivo(cpf, 'Notebook');
+//         res.status(200).json(result);
+//     } catch (error) {
+//         res.status(500).json({ message: 'Erro ao limpar notebook', error: error });
+//     }
+// });
+
+
+
 app.listen(port, () => {
-    console.log(`Servidor iniciado na porta ${port}`);
+    console.log(`Servidor rodando na porta`, port);
 });
